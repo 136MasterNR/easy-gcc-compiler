da818687f42d7229f027c231594443957fdfbaaf
136MasterNR <62593379+136MasterNR@users.noreply.github.com>
2023-10-23 20:36:49 +0300

    Update gcc.bat

--- gcc.bat
+++ gcc.bat
@@ -1,6 +1,6 @@
 @CHCP 65001 >NUL
 @VERIFY OFF
-@TITLE C Language : GCC-COMPILER v0.1.1
+@TITLE C Language : GCC-COMPILER v0.1.2
 @ECHO OFF
 
 
@@ -36,9 +36,12 @@ IF DEFINED ARG1 (
 	SET TARGET=%ARG1:"=%
 	SET "OUT=%ARG_OUT%.exe"
 	(
-		ECHO.SET TARGET=%ARG1:"=%
+		ECHO.SET "TARGET=%ARG1:"=%"
 		ECHO.SET "OUT=%ARG_OUT%.exe"
-	) > "%TMP_RMB%"
+	) > "%TMP_RMB%" || (
+		IF %LANGUAGE%==EN ECHO.[1;33mWARNING[0m: [1mUnable to remember last opened file.
+		IF %LANGUAGE%==GR ECHO.[1;33mΠΡΟΕΙΔΟΠΟΙΗΣΗ[0m: [1mΑδυναμία απομνημόνευσης του τελευταίου ανοιγμένου αρχείου.
+	)
 )
 
 
@@ -54,16 +57,28 @@ IF EXIST "%COMPILER_PATH%\bin\gcc.exe" (
 EXIT
 
 :INSTALLER
-ECHO.The compiler (GCC) is not installed on your computer.
-CHOICE /C YN /N /M "Do you want to install GCC essentials (Y/N)?"
+IF %LANGUAGE%==EN (
+	ECHO.The compiler (GCC) is not installed on your computer.
+	CHOICE /C YN /N /M "Do you want to install GCC essentials (Y/N)?"
+)
+IF %LANGUAGE%==GR (
+	ECHO.Ο μεταγλωττιστής (GCC) δεν είναι εγκατεστημένος στον υπολογιστή σας.
+	CHOICE /C YN /N /M "Θέλετε να εγκαταστήσετε τα απαραίτητα πακέτα του GCC στον υπολογιστή σας; (Y/N)"
+)
 
 IF ERRORLEVEL 2 (
 	:: If N is pressed, exit.
 	EXIT 1
 )
 
-IF %LANGUAGE%==EN TITLE Downloading ...
-IF %LANGUAGE%==GR TITLE Γίνεται λήψη ...
+IF %LANGUAGE%==EN (
+	TITLE Downloading ...
+	ECHO.Downloading ...
+)
+IF %LANGUAGE%==GR (
+	TITLE Γίνεται λήψη ...
+	ECHO.Γίνεται λήψη ...
+)
 IF EXIST ".\gcc.zip" DEL /F /Q ".\gcc.zip"
 CURL -S "https://img.comradeturtle.dev/assets/gcc.zip" --SSL-NO-REVOKE >".\gcc.zip"
 
@@ -94,6 +109,14 @@ IF %LANGUAGE%==GR ECHO.Καθαρισμός ...
 IF EXIST "%VBS%" DEL /F /Q "%VBS%"
 IF EXIST "%CD%\gcc.zip" DEL /F /Q "%CD%\gcc.zip"
 
+IF EXIST "%COMPILER_PATH%\bin\gcc.exe" (
+	IF %LANGUAGE%==EN ECHO.The installation has finished successfuly.
+	IF %LANGUAGE%==GR ECHO.Η εγκατάσταση ολοκληρώθηκε με επιτυχία.
+) ELSE (
+	IF %LANGUAGE%==EN ECHO.The installation wasn't successful.
+	IF %LANGUAGE%==GR ECHO.Η εγκατάσταση δεν ήταν επιτυχής.
+)
+PAUSE>NUL
 EXIT
 
 :COMPILER
@@ -108,14 +131,21 @@ DEL /Q "%OUT%" 2>NUL
 
 IF %LANGUAGE%==EN TITLE Compiling ...
 IF %LANGUAGE%==GR TITLE Συναρμολόγηση ...
-"C:\gcc\bin\gcc.exe" -Wall -Wextra -g3 "%TARGET%" -o "%OUT%"
+
+SET OUTPUT=FALSE
+
+:: The compiler can be configured from here. To run C++, change the -std switch to "c++17".
+FOR /F %%I IN ('START /B /W "" "%COMPILER_PATH%\bin\gcc.exe" -Wall -Wextra -g3 -O0 -std^=c11 "%TARGET%" -o "%OUT%"') DO (
+    SET "OUTPUT=TRUE"
+)
 
 IF EXIST "%OUT%" (
 	START CMD /C TITLE  ^& "%OUT%" ^& PAUSE^>NUL
+	IF %OUTPUT%==TRUE PAUSE>NUL
 ) ELSE (
 	ECHO.
 	IF %LANGUAGE%==EN ECHO.[1;31mFATAL[0m: [1mCompilation process failed, see above for hints.[0m
-	IF %LANGUAGE%==GR ECHO.[1;31mΣΦΑΛΜΑ[0m: [1mΑποτυχία διαδικασίας συναρμολόγησης, δείτε παραπάνω για περισσότερες πληροφορίες.[0m
+	IF %LANGUAGE%==GR ECHO.[1;31mΣΦΑΛΜΑ[0m: [1mΑποτυχία συναρμολόγησης, δείτε παραπάνω για περισσότερες πληροφορίες.[0m
 	PAUSE>NUL
 	EXIT 1
 )
